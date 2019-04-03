a159a1fcf4c5175862ddcd894fa6d173cc1be072
Antonio Salgado <antoniosalgado@Antonios-MacBook-Pro.local>
2019-04-03 19:07:08 -0300

    css adjusted, now need to populate the portfolio and go for curriculum page

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -2,4 +2,4 @@ var HTMLprojectCard = '<div class="col-12 col-sm-4 col-md-3 mt-4 mt-sm-3 project
 var HTMLprojectLink = '<a href="%data%" class="project-link"></a>';
 var HTMLprojectImage = '<img src="img/%data%">';
 var HTMLprojectName = '<div class="project-name"><p class="mt-1">%data%</p><ul class="tags-list pl-sm-0 pl-md-2 mt-1"></ul></div>';
-var HTMLprojectTag = '<li class="mr-2 ml-1"><a href="">%data%</a></li>';
+var HTMLprojectTag = '<li class="mr-2 ml-1"><a href="#">%data%</a></li>';
