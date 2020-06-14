2d6efbc5b2b0b57e11c48e82da93e935666128b8
antonio salgado <antonio@kiina.ai>
2020-06-13 22:28:35 -0300

    adjusted css project-link and project card img / adjusted index.html

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,4 +1,4 @@
-var HTMLprojectCard = '<div class="col-12 col-sm-4 col-md-3 mt-4 mt-sm-3 project-card"><div class="project-card-content"></div></div>';
+var HTMLprojectCard = '<div class="col-12 col-sm-6 col-md-4 col-xl-3 mt-4 mt-sm-3 project-card"><div class="project-card-content"></div></div>';
 var HTMLprojectLink = '<a href="%data%" class="project-link" target=”_blank”></a>';
 var HTMLprojectImage = '<img src="img/thumbs/%data%">';
 var HTMLprojectName = '<div class="project-name"><p class="mt-1">%data%</p><ul class="tags-list pl-sm-0 pl-md-2 mt-1"></ul></div>';
